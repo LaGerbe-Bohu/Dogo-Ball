ff7cc53d770905701d88c8f9708764121dbee313
SontotA <71493399+SontotA@users.noreply.github.com>
2022-09-27 10:20:56 +0200

    Physique et mouvement du frisbee

--- Project Windjammers/Assets/Scripts/Frisbee.cs
+++ Project Windjammers/Assets/Scripts/Frisbee.cs
@@ -1,11 +1,57 @@
 using UnityEngine;
 
-public class Frisbee
+public class Frisbee: MonoBehaviour
 {
 
     private Vector2 position;
     private Vector2 direction;
     private Joueur joueur;
+
+    public Rigidbody2D frisbeeBody;
+    public CircleCollider2D frisbeeCollider;
+    public Transform frisbeeTransform;
+
+    private void Start()
+    {
+        float random = Random.Range(0, 10);
+        if (random > 5)
+        {
+            direction = new Vector2(1, 5);
+        }
+        else direction = new Vector2(-1, 5);
+
+    }
+
+    private void FixedUpdate()
+    {
+        frisbeeBody.velocity += direction.normalized*Time.fixedDeltaTime*30;
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("TopWall"))
+        {
+            Vector2 newdir = new Vector2(frisbeeBody.velocity.x, -frisbeeBody.velocity.y);
+            newdir = Vector2.ClampMagnitude(newdir, 15);
+            frisbeeBody.velocity = newdir;
+            direction = new Vector2(direction.x, -direction.y);
+        }
+
+        if (collision.CompareTag("BottomWall"))
+        {
+            Vector2 newdir = new Vector2(frisbeeBody.velocity.x, -frisbeeBody.velocity.y);
+            newdir = Vector2.ClampMagnitude(newdir, 15);
+            frisbeeBody.velocity = newdir;
+            direction = new Vector2(direction.x, -direction.y);
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            frisbeeBody.velocity = new Vector2(0, 0);
+            direction = new Vector2(0, 0);
+        }
+    }
 
 
 }
